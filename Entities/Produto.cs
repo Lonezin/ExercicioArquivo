3be0209fbf13a76ae1c3d468435cc0cef86d152a
Vinicius Morais <vinidev.morais@gmail.com>
2024-03-05 13:26:56 -0300

    exercicio de arquivos

---
+++ Entities/Produto.cs
@@ -0,0 +1,20 @@
+namespace Atividade_Arquivo
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto (string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }        
+        public double ValorTotal()
+        {
+            return Preco * Quantidade;        
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using Atividade_Arquivo;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            System.Console.WriteLine("entre com o local do arquivo: ");
+            string localDoArquivo = Console.ReadLine(); //$"C:\Users\lonez\Desktop\Aula.csv"
+            try
+            {
+                string[] lines = File.ReadAllLines(localDoArquivo);
+                string sourceFolderPath = Path.GetDirectoryName(localDoArquivo);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string item in lines)
+                    {
+                        string [] campos = item.Split(",");
+                        string name = campos[0];
+                        double preco = double.Parse(campos[1],CultureInfo.InvariantCulture);
+                        int quantidade = int.Parse(campos[2]);
+
+                        Produto produto = new Produto(name, preco, quantidade);
+                        sw.WriteLine(produto.Nome + ", " + produto.ValorTotal().ToString("F2", CultureInfo.InvariantCulture) );
+                    }                    
+                }
+            }
+            catch (IOException e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
